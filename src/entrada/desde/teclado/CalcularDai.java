3fa9d67a6deaa07d2db475a4417a3147b536a7e5
JhonatanRoque <101343368+Crespin2@users.noreply.github.com>
2022-03-17 11:42:15 -0600

    Calcular Dai

---
+++ src/entrada/desde/teclado/CalcularDai.java
@@ -0,0 +1,37 @@
+
+package entrada.desde.teclado;
+
+import java.util.Scanner;
+
+
+public class CalcularDai {
+    public static void main(String[] args) {
+        Scanner leer= new Scanner(System.in);
+        double valorMaquina, valorFlete, valorSeguro;
+        System.out.println("Valoe de la maquina: ");
+        valorMaquina=leer.nextDouble();//Captura de datos en el tipo que se necesita
+        System.out.println("Valor del Flete: ");
+        valorFlete=leer.nextDouble();
+        System.out.println("Valor del Seguro: ");
+        valorSeguro=leer.nextDouble();
+        
+        double valorDai=CalcularDai.obtenerDai(valorMaquina,valorFlete,valorSeguro);
+        double valorConIva=CalcularDai.obtenerIva(valorDai);
+        
+        System.out.println("Valor del Dai: "+valorDai);
+        System.out.println("Valor del IVA: "+valorConIva);
+    }
+    //Crear el metodo para el calculo del DAI 
+    public static double obtenerDai(double valorMaquina, double valorFlete, double valorSeguro){
+        double dai;
+        dai=(valorMaquina-valorFlete-valorSeguro)*0.10;
+    return dai;
+    }
+    //Metodo para obtener el valor mas IVA
+    public static double obtenerIva(double dai){
+        double pagoConIva=0;
+        pagoConIva=dai*1.13;
+    return pagoConIva;
+    }
+    
+}
